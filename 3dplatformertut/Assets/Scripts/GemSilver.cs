7841ca5b5cbf9277ba64990c35a978f41579e495
jacob.hong <jacob.hong@gmail.com>
2020-10-25 02:18:59 -0700

    first

---
+++ 3dplatformertut/Assets/Scripts/GemSilver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GemSilver : MonoBehaviour
+{
+    public GameObject scoreBox;
+    public AudioSource collectSound;
+
+    private void OnTriggerEnter()
+    {
+        //scoreBox.GetComponent<Text>().text = "1000";
+        GlobalScore.currentScore += 1000;
+        collectSound.Play();
+        Destroy(gameObject);
+    }
+}
---
+++ 3dplatformertut/Assets/Scripts/RenderToLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RenderToLevel : MonoBehaviour
+{
+    public static int redirectToLevel = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (redirectToLevel == 1) {
+            SceneManager.LoadScene(redirectToLevel);
+	    }
+    }
+}
